7febd8986c84a5e957b209a44ddadb1c2a5ee704
RajuV2000 <rajuv.xworkz@gmail.com>
2021-12-02 01:09:56 +0530

    committed candy shop files

---
+++ js/getPrice.js
@@ -0,0 +1,107 @@
+function getPrice(candy){
+    if(candy=="Lemor"){
+        let v=document.getElementById("Lemor").value;
+        let price=v*5;
+        document.getElementById("LemorValue").innerHTML="Price for "+v+" Lemor Mint candy is Rs. "+price;
+    }
+    if(candy=="Organic"){
+        let v=document.getElementById("Organic").value;
+        let price=v*13;
+        document.getElementById("OrganicValue").innerHTML="Price for "+v+" Organic World jelly is Rs. "+price;
+    }
+    if(candy=="Wild"){
+        let v=document.getElementById("Wild").value;
+        let price=v*8;
+        document.getElementById("WildValue").innerHTML="Price for "+v+" Wild Berry candy is Rs. "+price;
+    }
+    if(candy=="Chupa"){
+        let v=document.getElementById("chupa").value;
+        let price=v*5;
+        document.getElementById("chupaValue").innerHTML="Price for "+v+" Chupa Chups Candy is Rs. "+price;
+    }
+    if(candy=="Berries"){
+        let v=document.getElementById("berries").value;
+        let price=v*28;
+        document.getElementById("berriesValue").innerHTML="Price for "+v+" Gummy Berrie's candy is Rs. "+price;
+    }
+    if(candy=="M&M"){
+        let v=document.getElementById("M&M").value;
+        let price=v*15;
+        document.getElementById("M&MValue").innerHTML="Price for "+v+" M&M's Choco Candy is Rs. "+price;
+    }
+    if(candy=="Kacchi"){
+        let v=document.getElementById("Kacchi").value;
+        let price=v*8;
+        document.getElementById("KacchiValue").innerHTML="Price for "+v+" Kacchi Keri Candy is Rs. "+price;
+    }
+    if(candy=="poppins"){
+        let v=document.getElementById("poppins").value;
+        let price=v*5;
+        document.getElementById("poppinsValue").innerHTML="Price for "+v+" Parle Poppins is Rs. "+price;
+    }
+    if(candy=="kala"){
+        let v=document.getElementById("kala").value;
+        let price=v*10;
+        document.getElementById("kalaValue").innerHTML="Price for "+v+" Kala Khatta is Rs. "+price;
+    }
+    if(candy=="alpenliebe"){
+        let v=document.getElementById("alpenliebe").value;
+        let price=v*2;
+        document.getElementById("alpenliebeValue").innerHTML="Price for "+v+" Alpenliebe Gold is Rs. "+price;
+    }
+    if(candy=="derby"){
+        let v=document.getElementById("derby").value;
+        let price=v*6;
+        document.getElementById("derbyValue").innerHTML="Price for "+v+" Derby Yummy is Rs. "+price;
+    }
+    if(candy=="nature"){
+        let v=document.getElementById("nature").value;
+        let price=v*3;
+        document.getElementById("natureValue").innerHTML="Price for "+v+" Nature homies candy is Rs. "+price;
+    }
+    if(candy=="tangy"){
+        let v=document.getElementById("tangy").value;
+        let price=v*4;
+        document.getElementById("tangyValue").innerHTML="Price for "+v+" Tangy candy is Rs. "+price;
+    }
+    if(candy=="nathans"){
+        let v=document.getElementById("nathans").value;
+        let price=v*6;
+        document.getElementById("nathansValue").innerHTML="Price for "+v+" Nathan's orange candy is Rs. "+price;
+    }
+    if(candy=="rola"){
+        let v=document.getElementById("rola").value;
+        let price=v*10;
+        document.getElementById("rolaValue").innerHTML="Price for "+v+" Rola Cola is Rs. "+price;
+    }
+
+    if(candy=="coal"){
+        let v=document.getElementById("coal").value;
+        let price=v*12;
+        document.getElementById("coalValue").innerHTML="Price for "+v+" Coal Candy is Rs. "+price;
+    }
+
+    if(candy=="cadbury"){
+        let v=document.getElementById("cadbury").value;
+        let price=v*10;
+        document.getElementById("cadburyValue").innerHTML="Price for "+v+" Cadbury gems is Rs. "+price;
+    }
+
+    if(candy=="fruitella"){
+        let v=document.getElementById("fruitella").value;
+        let price=v*18;
+        document.getElementById("fruitellaValue").innerHTML="Price for "+v+" Fruitella candy is Rs. "+price;
+    }
+
+    if(candy=="skittels"){
+        let v=document.getElementById("skittels").value;
+        let price=v*7;
+        document.getElementById("skittelsValue").innerHTML="Price for "+v+" Skittels candy is Rs."+price;
+    }
+
+    if(candy=="lemon"){
+        let v=document.getElementById("lemon").value;
+        let price=v*5;
+        document.getElementById("lemonValue").innerHTML="Price for "+v+" Lemon Candy is Rs."+price;
+    }
+}
